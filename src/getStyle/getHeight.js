74db07ad9a09c42ad0bde850e52d5aa89b934f90
Nick Johnson <nickjohnson.dev@gmail.com>
2017-10-10 17:39:41 -0500

    fix: Fix bug where popup would be full width/height when target was partially offscreen.

--- src/getStyle/getHeight.js
+++ src/getStyle/getHeight.js
@@ -6,31 +6,8 @@ export function getHeight(args) {
   const containerHeight = getOr(windowHeight, 'containerHeight', args);
   const popupRect = invoke('popupEl.getBoundingClientRect', args);
   const popupHeight = getOr(0, 'height', popupRect);
-  const popupVertical = getOr('top', 'popupVertical', args);
-  const targetRect = invoke('targetEl.getBoundingClientRect', args);
-  const targetBottom = getOr(0, 'bottom', targetRect);
-  const targetTop = getOr(0, 'top', targetRect);
-  const targetVertical = getOr('top', 'targetVertical', args);
 
-  if (popupVertical === 'bottom') {
-    const baseBottom = containerHeight - (targetVertical === 'top' ? targetTop : targetBottom);
-    const topMostPoint = Math.max(baseBottom + popupHeight, containerHeight); // 100
-    const adjustedBottom = baseBottom - (topMostPoint - containerHeight);
-
-    if (adjustedBottom < 0) return containerHeight;
-
-    return '';
-  }
-
-  if (popupVertical === 'top') {
-    const baseTop = targetVertical === 'bottom' ? targetBottom : targetTop;
-    const bottomMostPoint = Math.max(baseTop + popupHeight, containerHeight); // 100
-    const adjustedTop = baseTop - (bottomMostPoint - containerHeight);
-
-    if (adjustedTop < 0) return containerHeight;
-
-    return '';
-  }
+  if (popupHeight > containerHeight) return containerHeight;
 
   return '';
 }
--- src/getStyle/getWidth.js
+++ src/getStyle/getWidth.js
@@ -6,31 +6,8 @@ export function getWidth(args) {
   const containerWidth = getOr(windowWidth, 'containerWidth', args);
   const popupRect = invoke('popupEl.getBoundingClientRect', args);
   const popupWidth = getOr(0, 'width', popupRect);
-  const popupHorizontal = getOr('left', 'popupHorizontal', args);
-  const targetRect = invoke('targetEl.getBoundingClientRect', args);
-  const targetRight = getOr(0, 'right', targetRect);
-  const targetLeft = getOr(0, 'left', targetRect);
-  const targetHorizontal = getOr('left', 'targetHorizontal', args);
 
-  if (popupHorizontal === 'right') {
-    const baseRight = containerWidth - (targetHorizontal === 'left' ? targetLeft : targetRight);
-    const leftMostPoint = Math.max(baseRight + popupWidth, containerWidth); // 100
-    const adjustedRight = baseRight - (leftMostPoint - containerWidth);
-
-    if (adjustedRight < 0) return containerWidth;
-
-    return '';
-  }
-
-  if (popupHorizontal === 'left') {
-    const baseLeft = targetHorizontal === 'right' ? targetRight : targetLeft;
-    const rightMostPoint = Math.max(baseLeft + popupWidth, containerWidth); // 100
-    const adjustedLeft = baseLeft - (rightMostPoint - containerWidth);
-
-    if (adjustedLeft < 0) return containerWidth;
-
-    return '';
-  }
+  if (popupWidth > containerWidth) return containerWidth;
 
   return '';
 }
